a7981f4345b9c03483360ccc48e71657a8c20ff8
SwapnilVicky <swapnil.motoe@gmail.com>
2024-01-28 05:48:17 +0530

    PixelPropsUtils: Enable gameprops by default

--- core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
+++ core/java/com/android/internal/util/crdroid/PixelPropsUtils.java
@@ -273,9 +273,6 @@ public final class PixelPropsUtils {
             }
         } else {
 
-            if (!SystemProperties.getBoolean(SPOOF_PIXEL_GAMES, false))
-                return;
-
             if (Arrays.asList(packagesToChangeROG6).contains(packageName)) {
                 if (DEBUG) Log.d(TAG, "Defining props for: " + packageName);
                 for (Map.Entry<String, Object> prop : propsToChangeROG6.entrySet()) {
